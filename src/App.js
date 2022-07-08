3d4e9c159e2655b286e3e93a06fc3f2dd7e246d6
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-07 21:52:39 -0400

    add hero into app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Hero from "./components/Hero";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       Dan's site in test
     </div>
   );
