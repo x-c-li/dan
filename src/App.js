afd4139ba7eb7484b852cf62125d0f22aede6713
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-07 21:06:24 -0400

    put navbar component in app file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">Dan's site in test</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      Dan's site in test
+    </div>
+  );
 }
 
 export default App;
