5907b2f842436c5c6dbb3ff0f70e5853281ca4f8
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-17 15:49:31 -0400

    clean code

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,21 +2,17 @@ import firebase from "firebase/compat/app"; //doing import firebase from 'fireba
 import "firebase/compat/auth";
 import "firebase/compat/database";
 import "firebase/compat/firestore";
-
-import Axios from "axios";
+// import { initializeApp } from "firebase/app";
 
 // Initialize Firebase
-let config = {
+const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-};
+});
 
-const firebaseApp = firebase.initializeApp(config);
-const db = firebaseApp.firestore(); //youtube tutorial
-// const db = firebase.firestore(); //website tutorial
-
-export { Axios, db };
+const db = firebaseApp.firestore();
+export { db };
