78ae0628622525af787661e8fe17ce9f4290d0f5
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-17 12:32:51 -0400

    allow contact form on app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ContactForm from "./components/ContactForm";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       Dan's site in test
+      <ContactForm />
     </div>
   );
 }
