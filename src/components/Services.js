650ff9c84b74946f435c1da16bba762f9196bda3
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-07-28 20:58:54 -0400

    changed content of services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,10 @@
 import "./Services.scss";
 
 function Services() {
-    return (<div className="service-container">
-
-
-            <div className="serviceBox">
-                <h3>ServiceOne</h3>
+  return (
+    <div className="service-container">
+      <div className="serviceBox">
+        {/* <h3>ServiceOne</h3>
                 <div className="image-box">
                     <img
                         src="https://images.unsplash.com/photo-1518534543674-5933a2307dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
@@ -49,12 +48,15 @@ function Services() {
                 </div>
                 <div className="service-price">
                     <h4>$4.20</h4>
-                </div>
-
-            </div>
-
-        </div>
-    )
+                </div> */}
+        <h1>Services:</h1>
+        <h4>Liquid damage repair</h4>
+        <h4>No power repair</h4>
+        <h4>Device randomly restarting</h4>
+        <h4>Motherboard/logic board repair</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
