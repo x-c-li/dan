f5089016bf44253bf9855889bb647b8cd62c8615
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-08-03 00:51:15 -0400

    change website name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.scss";
 function Navbar() {
   return (
     <div className="Navbar">
-      <div className="navbar-logo">Dan's Repair</div>
+      <div className="navbar-logo">R3 Technology</div>
       {/* <div className="navbar-links">
         <div className="navbar-temp">Services</div>
         <div className="navbar-temp">Contact Me</div> 
