91197991e7aef11b7eadc22396be8a84f047dd61
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2022-08-12 19:41:49 -0400

    working email using formsubmit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,42 +1,46 @@
 import React, { useState } from "react";
 import "./ContactForm.scss";
-import { db } from "../firebase/firebaseConfig";
+// import { db } from "../firebase/firebaseConfig";
 
 function ContactForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState("");
 
-  const [loader, setLoader] = useState(false);
+  // const [loader, setLoader] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoader(true);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setLoader(true);
 
-    //db collection in firebase - we named it contacts here
-    db.collection("email")
-      .add({
-        name: name,
-        email: email,
-        message: message,
-      })
-      .then(() => {
-        setLoader(false);
-        alert("Your message has been submitted");
-      })
-      .catch((error) => {
-        alert(error.message);
-        setLoader(false);
-      });
+  //   //db collection in firebase - we named it contacts here
+  //   db.collection("email")
+  //     .add({
+  //       name: name,
+  //       email: email,
+  //       message: message,
+  //     })
+  //     .then(() => {
+  //       setLoader(false);
+  //       alert("Your message has been submitted");
+  //     })
+  //     .catch((error) => {
+  //       alert(error.message);
+  //       setLoader(false);
+  //     });
 
-    setName("");
-    setEmail("");
-    setMessage("");
-  };
+  //   setName("");
+  //   setEmail("");
+  //   setMessage("");
+  // };
 
   return (
     <div className="container">
-      <form className="ContactForm" onSubmit={handleSubmit}>
+      <form
+        className="ContactForm"
+        action="https://formsubmit.co/a69aa978cd7f2af9071bd0bbee8d0fe3"
+        method="POST"
+      >
         <div className="form-title">
           <label>Contact Me</label>
         </div>
@@ -46,8 +50,8 @@ function ContactForm() {
           id="name"
           name="name"
           placeholder="Your name.."
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          // value={name}
+          // onChange={(e) => setName(e.target.value)}
         ></input>
 
         <label>Email Address</label>
@@ -56,8 +60,9 @@ function ContactForm() {
           id="email"
           name="email"
           placeholder="Your email address..."
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          required
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
         ></input>
 
         <label>Message</label>
@@ -65,12 +70,12 @@ function ContactForm() {
           id="message"
           name="message"
           placeholder="Write something..."
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          // value={message}
+          // onChange={(e) => setMessage(e.target.value)}
         ></textarea>
 
         <button
-          style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
+          // style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
           type="submit"
         >
           Submit
